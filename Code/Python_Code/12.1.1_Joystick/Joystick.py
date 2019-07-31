debb930d936ba8c4e38ac041933c62c784fab1cc
Mitchell Hamann <captain@voyager.local>
2019-07-31 18:49:51 -0500

    pep8

--- Code/Python_Code/12.1.1_Joystick/Joystick.py
+++ Code/Python_Code/12.1.1_Joystick/Joystick.py
@@ -10,41 +10,47 @@ import smbus
 import time
 
 address = 0x48
-bus=smbus.SMBus(1)
-cmd=0x40
-Z_Pin = 12      #define pin for Z_Pin
-def analogRead(chn):        #read ADC value
-    bus.write_byte(address,cmd+chn)
+bus = smbus.SMBus(1)
+cmd = 0x40
+Z_Pin = 12  # define pin for Z_Pin
+
+
+def analogRead(chn):  # read ADC value
+    bus.write_byte(address, cmd+chn)
     value = bus.read_byte(address)
     value = bus.read_byte(address)
     #value = bus.read_byte_data(address,cmd+chn)
     return value
-    
+
+
 def analogWrite(value):
-    bus.write_byte_data(address,cmd,value)  
+    bus.write_byte_data(address, cmd, value)
+
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)        
-    GPIO.setup(Z_Pin,GPIO.IN,GPIO.PUD_UP)   #set Z_Pin to pull-up mode
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(Z_Pin, GPIO.IN, GPIO.PUD_UP)  # set Z_Pin to pull-up mode
+
+
 def loop():
-    while True:     
-        val_Z = GPIO.input(Z_Pin)       #read digital quality of axis Z
-        val_Y = analogRead(0)           #read analog quality of axis X and Y
+    while True:
+        val_Z = GPIO.input(Z_Pin)  # read digital quality of axis Z
+        val_Y = analogRead(0)  # read analog quality of axis X and Y
         val_X = analogRead(1)
-        print ('value_X: %d ,\tvlue_Y: %d ,\tvalue_Z: %d'%(val_X,val_Y,val_Z))
+        print('value_X: %d ,\tvlue_Y: %d ,\tvalue_Z: %d' %
+              (val_X, val_Y, val_Z))
         time.sleep(0.01)
+
 
 def destroy():
     bus.close()
     GPIO.cleanup()
-    
+
+
 if __name__ == '__main__':
-    print ('Program is starting ... ')
+    print('Program is starting ... ')
     setup()
     try:
         loop()
     except KeyboardInterrupt:
         destroy()
-
-		
-	
