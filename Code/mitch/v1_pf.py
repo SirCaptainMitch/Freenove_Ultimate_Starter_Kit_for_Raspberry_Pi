562dc49f64d5674fcd3c8bcf1a95066b5cf37706
Mitchell Hamann <captain@voyager.local>
2019-10-05 17:01:01 -0500

    testing

--- Code/mitch/v1_pf.py
+++ Code/mitch/v1_pf.py
@@ -52,7 +52,7 @@ def motorStop():
 def loop():
     while True:
         if GPIO.input(buttonPin) == GPIO.LOW:
-            PositionCount += 1
+            Positioncount += 1
             print('Button pressed ...')
             moveSteps(1, 3, 50)
 
